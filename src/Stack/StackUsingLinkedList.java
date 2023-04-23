800224e7eacb2ed051b8d6538edc7b8c96924713
Sneha Tarnekar <snehatarnekar@gmail.com>
2023-04-23 19:40:50 +0530

    Implement stack using Linked List

---
+++ src/Stack/StackUsingLinkedList.java
@@ -0,0 +1,63 @@
+package Stack;
+
+import LinkedList.SinglyLinkedList.Node;
+
+public class StackUsingLinkedList {
+	
+	public static void main(String[] args) {
+		StackUsingLinkedList stack = new StackUsingLinkedList();
+		stack.push(5);
+		stack.push(3);
+		while(!stack.isEmpty()) {
+			System.out.println(stack.peak());
+			stack.pop();
+		}		
+	}
+
+	class Node {
+		public int data;
+		public Node nextNode;
+	}
+
+	Node head;
+	int size;
+
+	public StackUsingLinkedList() {
+		head = null;
+		size = 0;
+	}
+
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	public void push(int data) {
+		Node temp = new Node();
+		temp.data = data;
+		temp.nextNode = head;
+		size++;
+		head = temp;
+	}
+
+	public int peak() {
+		if (isEmpty()) {
+			throw new RuntimeException("Stack underflow");
+		}
+		return head.data;
+	}
+
+	public int pop() {
+		if (isEmpty()) {
+			throw new RuntimeException("Stack underflow");
+		}
+		int response = head.data;
+		head = head.nextNode;
+		size--;
+		return response;
+	}
+
+	public int size() {
+		return size;
+	}
+
+}
