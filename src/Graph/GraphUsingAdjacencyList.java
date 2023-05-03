9f5dd28d2cf815c7d2ff0efdec7d0a1add42da0f
Sneha Tarnekar <snehatarnekar@gmail.com>
2023-05-03 22:25:38 +0530

    Implement graph using Adjacency List

---
+++ src/Graph/GraphUsingAdjacencyList.java
@@ -0,0 +1,35 @@
+package Graph;
+
+import LinkedList.DoublyLinkedList;
+
+public class GraphUsingAdjacencyList {
+	int vertices;
+	DoublyLinkedList<Integer> adjList[];
+	
+	@SuppressWarnings("unchecked")
+	public GraphUsingAdjacencyList(int vertices) {
+		this.vertices = vertices;
+		adjList = new DoublyLinkedList[vertices];
+		
+		for(int i=0; i<vertices; i++) {
+			adjList[i] = new DoublyLinkedList<>();
+		}
+	}
+	
+	public void addEdge(int source, int destination) {
+		 if (source < vertices && destination < vertices) {
+			 this.adjList[source].insertAtTail(destination);
+			 this.adjList[destination].insertAtTail(destination); 	// For undirected graph 
+		 }
+	}
+
+	public static void main(String[] args) {
+		GraphUsingAdjacencyList graph = new GraphUsingAdjacencyList(5);
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 5);
+		graph.addEdge(1, 4);
+		graph.addEdge(1, 3);
+	}
+
+}
