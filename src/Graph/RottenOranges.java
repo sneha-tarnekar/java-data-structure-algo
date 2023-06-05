7270c286e5541e5fef60bc35391cb60ec3c50b25
Sneha Tarnekar <snehatarnekar@gmail.com>
2023-06-05 23:52:18 +0530

    Added problem statement with solution

--- src/Graph/RottenOranges.java
+++ src/Graph/RottenOranges.java
@@ -3,6 +3,16 @@ package Graph;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+ *   Given a grid of dimension nxm where each cell in the grid can have values 0, 1 or 2 which has the following meaning:
+ *   0 : Empty cell
+ *   1 : Cells have fresh oranges
+ *   2 : Cells have rotten oranges
+ *
+ *   We have to determine what is the minimum time required to rot all oranges.
+ *   A rotten orange at index [i,j] can rot other fresh orange at indexes [i-1,j], [i+1,j], [i,j-1], [i,j+1] (up, down, left and right) in unit time.
+ */
+
 public class RottenOranges {
 
     public static int orangesRotting(int[][] input) {
