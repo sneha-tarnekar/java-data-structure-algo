091382743354aaa690ed5c8a061c7cb378cd43ee
Sneha Tarnekar <snehatarnekar@gmail.com>
2023-06-08 23:30:16 +0530

    Add problem statement

--- src/Graph/FloodFill.java
+++ src/Graph/FloodFill.java
@@ -1,5 +1,30 @@
 package Graph;
 
+/*
+ *  An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+ *
+ *  You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+ *
+ *  To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel,
+ *  plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+ *  Return the modified image after performing the flood fill.
+ *
+ *  Input:
+ *  sr=1, sc=1, newColor=2
+ *  image = {
+ *               {1, 1, 1},
+ *               {1, 1, 0},
+ *               {0, 0, 1}
+ *          }
+ *
+ *  Output: {
+ *               {2, 2, 2},
+ *               {2, 2, 0},
+ *               {0, 0, 1}
+ *          }
+ *
+ * */
+
 public class FloodFill {
 
     public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
