322dd10c4d3f323c5a40c69e80ebcfc38eb64111
Sneha Tarnekar <snehatarnekar@gmail.com>
2023-07-04 23:53:24 +0530

    Print Diamond Pattern

---
+++ src/Pattern/DiamondPattern.java
@@ -0,0 +1,34 @@
+package Pattern;
+
+public class DiamondPattern {
+
+    public static void printDiamond(int n) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n-i-1; j++) {
+                System.out.print(" ");
+            }
+
+            for(int j=0; j<=i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                System.out.print(" ");
+            }
+
+            for(int j=0; j<=n-i-1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printDiamond(8);
+    }
+}
