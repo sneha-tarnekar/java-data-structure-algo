ff8ef57a80a51cd52b62875cb7f9ec8377d99135
Sneha Tarnekar <snehatarnekar@gmail.com>
2023-07-05 23:41:24 +0530

    Remove duplicate elements from sorted array

---
+++ src/Arrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.TreeSet;
+
+/*
+ *   Given a sorted array A[] of size N, delete all the duplicated elements from A[].
+ *   Modify the array such that if there are X distinct elements in it
+ *   then the first X positions of the array should be filled with them in increasing order
+ *   and return the number of distinct elements in the array.
+ */
+public class RemoveDuplicatesFromSortedArray {
+
+    static int remove_duplicate(int A[], int N) {
+        TreeSet<Integer> set = new TreeSet();
+        for (int i = 0; i < N; i++) {
+            set.add(A[i]);
+        }
+
+        int k = set.size();
+
+        A[0] = set.stream().findFirst().get();
+        int j = 0;
+        for (int ele : set) {
+            A[j++] = ele;
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {2, 3, 3, 3, 4, 4};
+        int uniqueElements = remove_duplicate(input, input.length);
+        System.out.println("After removing all the duplicates, number of array elements remained : " + uniqueElements);
+    }
+}
