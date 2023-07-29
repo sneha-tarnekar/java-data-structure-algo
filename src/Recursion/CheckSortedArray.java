be3e7277b5186d92df69bd6e583956c2bf8785df
Sneha2405 <snehatarnekar@gmail.com>
2023-07-29 14:04:23 +0530

    fix check sorted array using recursion

--- src/Recursion/CheckSortedArray.java
+++ src/Recursion/CheckSortedArray.java
@@ -3,13 +3,13 @@ package Recursion;
 public class CheckSortedArray {
 
     private static boolean isSorted(int[] input, int len) {
-        if(len == 0 || len == input.length) return true;
-        if(input[len]<input[len-1]) return false;
-        return true && isSorted(input, len-1);
+        if(len == 0 || len == 1) return true;
+        if(input[len-1]<input[len-2]) return false;
+        return isSorted(input, len-1);
     }
 
     public static void main(String[] args) {
-        int[] input = {1,2,3,8,5,6,7};
+        int[] input = {1,2,5,6,7};
         boolean result = isSorted(input, input.length);
         System.out.println("Is sorted : " + result);
     }
