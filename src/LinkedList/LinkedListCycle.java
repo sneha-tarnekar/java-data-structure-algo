540b45fb0fae8668ca58dbbbc397bcaf1a74ac0b
sneha <snehatarnekar@gmail.com>
2023-09-07 23:13:43 +0530

    Check if there is a cycle in the linked list

---
+++ src/LinkedList/LinkedListCycle.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
