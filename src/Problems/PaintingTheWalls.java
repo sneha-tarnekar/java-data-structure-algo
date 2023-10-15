ee01bac1756bef0b2817673406b3a711fe3f3690
sneha <snehatarnekar@gmail.com>
2023-10-15 22:51:26 +0530

    Fix the solution

--- src/Problems/PaintingTheWalls.java
+++ src/Problems/PaintingTheWalls.java
@@ -1,5 +1,7 @@
 package Problems;
 
+import java.util.Arrays;
+
 /*
     You are given two 0-indexed integer arrays, cost and time, of size n representing the costs and the time taken to paint n different walls respectively. There are two painters available:
     A paid painter that paints the ith wall in time[i] units of time and takes cost[i] units of money.
@@ -9,25 +11,21 @@ package Problems;
 public class PaintingTheWalls {
 
     public static int paintWalls(int[] cost, int[] time) {
-        int n = cost.length;
-        int[] dp = new int[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            dp[i] = cost[i - 1] + dp[i - 1];
-            int totalTime = time[i - 1];
-
-            for (int j = i - 2; j >= 0; j--) {
-                totalTime += time[j];
-                dp[i] = Math.min(dp[i], dp[j] + totalTime);
+        int n = time.length;
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp, (int) 1e9);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > 0; j--) {
+                dp[j] = Math.min(dp[j], dp[Math.max(j - time[i] - 1, 0)] + cost[i]);
             }
         }
-
         return dp[n];
     }
 
     public static void main(String[] args) {
-        int[] cost = {2, 3, 4, 2};
-        int[] time = {1, 1, 1, 1};
+        int[] cost = {1, 2, 3, 2};
+        int[] time = {1, 2, 3, 2};
 
         int output = paintWalls(cost, time);
         System.out.println("Output : " + output);
