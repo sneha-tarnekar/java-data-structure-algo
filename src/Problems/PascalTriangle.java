d4bd0a7e72bd7caba02879b9fd5e075c038852fc
sneha <snehatarnekar@gmail.com>
2023-10-17 23:18:23 +0530

    Print pascal triangle

--- src/Problems/PascalTriangle.java
+++ src/Problems/PascalTriangle.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class PascalTriangle {
 
-    public List<List<Integer>> generate(int numRows) {
+    public static List<List<Integer>> generate(int numRows) {
         LinkedList<List<Integer>> resultList = new LinkedList();
         resultList.add(new LinkedList());
         resultList.getLast().add(1);
@@ -21,6 +21,26 @@ public class PascalTriangle {
         }
 
         return resultList;
+    }
+
+    public static void print(List<List<Integer>> list) {
+        for(int i=0; i<list.size(); i++) {
+            for(int j=0; j<list.size()-i; j++) {
+                System.out.print(" ");
+            }
+
+            for(int j=0; j<list.get(i).size(); j++) {
+                System.out.print(list.get(i).get(j)+ " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int numRows = 5;
+        List<List<Integer>> output = generate(numRows);
+        print(output);
     }
 
 }
