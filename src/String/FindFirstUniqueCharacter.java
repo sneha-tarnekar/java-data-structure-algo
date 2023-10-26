df06408c100409f2dd617e0441246022adc9aa86
sneha <snehatarnekar@gmail.com>
2023-10-26 23:00:42 +0530

    Add problem statement for first unique character

--- src/String/FindFirstUniqueCharacter.java
+++ src/String/FindFirstUniqueCharacter.java
@@ -1,8 +1,12 @@
 package String;
 
+/*
+    Given a string s, find the first non-repeating character in it and return its index.
+    If it does not exist, return -1.
+ */
 public class FindFirstUniqueCharacter {
 
-    public int firstUniqChar(String s) {
+    public static int firstUniqChar(String s) {
         char[] arr = s.toCharArray();
 
         boolean isRepeated = false;
@@ -22,6 +26,12 @@ public class FindFirstUniqueCharacter {
         }
 
         return -1;
+    }
+
+    public static void main(String[] args) {
+        String input = "lovetocode";
+        int output = firstUniqChar(input);
+        System.out.println("Output : " + output);
     }
 
 }
