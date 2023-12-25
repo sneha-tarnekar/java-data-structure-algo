51d77253971c01032abed8eec880e613deb62e8a
sneha <snehatarnekar@gmail.com>
2023-12-25 16:32:40 +0530

    Updated code to match all test cases

--- src/String/CheckIsAnagramString.java
+++ src/String/CheckIsAnagramString.java
@@ -1,27 +1,36 @@
 package String;
 
+import java.util.Arrays;
+
+//  Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+//  An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+//  typically using all the original letters exactly once.
 public class CheckIsAnagramString {
 
     public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
 
-        for (char ch : s.toCharArray()) {
-            if (!t.contains(String.valueOf(ch))) {
-                return false;
-            }
-        }
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
 
-        return true;
+        return Arrays.equals(sChars, tChars);
     }
 
     public static void main(String[] args) {
+        // Case 1
         String s = "anagram";
         String t = "nagaram";
 
         boolean output = isAnagram(s, t);
-        System.out.println("Is Anagram : " + output);
+        System.out.println(String.format("Is String '%s' and '%s' Anagram : %s", s, t, output));
+
+        // Case 2
+        String s1 = "aacc";
+        String t1 = "ccab";
+
+        boolean output1 = isAnagram(s1, t1);
+        System.out.println(String.format("Is String '%s' and '%s' Anagram : %s", s1, t1, output1));
     }
 
 }
