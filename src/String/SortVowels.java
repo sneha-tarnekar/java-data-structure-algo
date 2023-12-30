18a0f1b8764efed43a9fa9b9b449918139db70dc
sneha <snehatarnekar@gmail.com>
2023-12-30 21:34:01 +0530

    Sort vowels in the ascending order of their ASCII values

---
+++ src/String/SortVowels.java
@@ -0,0 +1,48 @@
+package String;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+//  Given a 0-indexed string s,permute s to get a new string t such that:
+//  All consonants remain in their original places.More formally,if there is an index i with 0<=i<s.length such that s[i]is a consonant,then t[i]=s[i].
+//  The vowels must be sorted in the nondecreasing order of their ASCII values.More formally,for pairs of indices i,j with 0<=i<j<s.length such that s[i]and s[j]are vowels,then t[i]must not have a higher ASCII value than t[j].
+//  Return the resulting string.
+//  The vowels are'a','e','i','o',and'u',and they can appear in lowercase or uppercase.Consonants comprise all letters that are not vowels.
+public class SortVowels {
+
+    public String sortVowels(String s) {
+        Character[] fixedVowels = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
+        Set<Character> vowelsSet = new HashSet<>(Arrays.asList(fixedVowels));
+
+        char[] sArr = s.toCharArray();
+
+        int count = 0;
+        for (char ch : sArr) {
+            if (vowelsSet.contains(ch))
+                count++;
+        }
+
+        char[] vowels = new char[count];
+        int vIndex = 0;
+        for (char ch : sArr) {
+            if (vowelsSet.contains(ch))
+                vowels[vIndex++] = ch;
+        }
+
+        Arrays.sort(vowels);
+
+        StringBuilder ans = new StringBuilder();
+        vIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (vowelsSet.contains(sArr[i])) {
+                ans.append(vowels[vIndex++]);
+            } else {
+                ans.append(sArr[i]);
+            }
+        }
+
+        return ans.toString();
+    }
+
+}
