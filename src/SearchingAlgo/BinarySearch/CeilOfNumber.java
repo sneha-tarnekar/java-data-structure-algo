282a71b4d8df5c6c8a3040c1ac50a624e4579064
sneha <snehatarnekar@gmail.com>
2024-02-27 09:23:00 +0530

    Find ceiling of a number

---
+++ src/SearchingAlgo/BinarySearch/CeilOfNumber.java
@@ -0,0 +1,37 @@
+package SearchingAlgo.BinarySearch;
+
+public class CeilOfNumber {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 24;
+        int output = ceiling(arr, target);
+        System.out.println(output);
+
+    }
+
+    private static int ceiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target > arr[arr.length - 1]) {
+                return Integer.MAX_VALUE;
+            }
+
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return arr[mid];
+            }
+        }
+
+        return arr[start];
+    }
+
+}
