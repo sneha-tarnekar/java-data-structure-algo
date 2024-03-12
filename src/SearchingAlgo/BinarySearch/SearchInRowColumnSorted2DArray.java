9e8150b0ee45983d9b84f3188b98ec80be2a4a57
sneha <snehatarnekar@gmail.com>
2024-03-12 10:06:27 +0530

    Search in row and column wise sorted 2D array

---
+++ src/SearchingAlgo/BinarySearch/SearchInRowColumnSorted2DArray.java
@@ -0,0 +1,45 @@
+package SearchingAlgo.BinarySearch;
+
+import java.util.Arrays;
+
+public class SearchInRowColumnSorted2DArray {
+
+    public static void main(String[] args) {
+
+        // Array sorted row and column wise
+        int[][] arr = {
+                {1, 2, 4},
+                {3, 5, 9},
+                {7, 8, 11}
+        };
+
+        int target = 8;
+
+        int[] output = search(arr, target);
+        System.out.println("Output : " + Arrays.toString(output));
+    }
+
+    public static int[] search(int[][] arr, int target) {
+        int row = 0;
+        int col = arr.length - 1;
+
+        while (row <= arr.length - 1 && col >= 0) {
+            int mid = arr[row][col];
+
+            if (mid == target) {
+                return new int[]{row, col};
+            }
+
+            if (target > mid) {
+                row++;
+            } else {
+                col--;
+            }
+
+        }
+
+        return new int[]{-1, -1};
+    }
+
+
+}
