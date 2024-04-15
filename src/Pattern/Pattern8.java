ee9e19980f1dabe39972e0fd38816aa578d92232
sneha <snehatarnekar@gmail.com>
2024-04-15 09:42:05 +0530

    Print star pattern8

---
+++ src/Pattern/Pattern8.java
@@ -0,0 +1,33 @@
+package Pattern;
+
+/*
+
+         *
+       * * *
+     * * * * *
+   * * * * * * *
+ * * * * * * * * *
+
+ */
+public class Pattern8 {
+
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
