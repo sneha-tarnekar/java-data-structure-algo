811872ddfca188e8ce0220d0805208e0cf07ba51
sneha <snehatarnekar@gmail.com>
2024-05-05 11:04:30 +0530

    Print star pattern7

---
+++ src/Pattern/Pattern7.java
@@ -0,0 +1,30 @@
+package Pattern;
+
+/*
+
+ * * * * *
+   * * * *
+     * * *
+       * *
+         *
+
+ */
+public class Pattern7 {
+
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
