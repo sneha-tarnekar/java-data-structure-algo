50594ea59cae7198eb85c0fe870c5a0b37ff7766
sneha <snehatarnekar@gmail.com>
2024-05-08 08:05:50 +0530

    Print pattern9

---
+++ src/Pattern/Pattern9.java
@@ -0,0 +1,30 @@
+package Pattern;
+
+/*
+
+ * * * * * * * * *
+   * * * * * * *
+     * * * * *
+       * * *
+         *
+
+ */
+public class Pattern9 {
+
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i * 2; j < n * 2 - 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
