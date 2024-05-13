54c960a84e8e7d95e9b32454f24e86db51b3fad6
sneha <snehatarnekar@gmail.com>
2024-05-13 09:48:55 +0530

    Implement preOrder, inOrder and postOrder traversal

--- src/Tree/BinarySearchTree.java
+++ src/Tree/BinarySearchTree.java
@@ -104,4 +104,46 @@ public class BinarySearchTree {
         display(node.right, "Right node of " + node.value + " : ");
     }
 
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.print(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.value + " ");
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + " ");
+    }
+
 }
