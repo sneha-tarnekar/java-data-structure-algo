e69cc46cc8fe6eacd317903a0bdcef8602910856
sneha <snehatarnekar@gmail.com>
2024-05-14 21:49:58 +0530

    Add test case to check Binary Tree Implementation

--- src/Tree/Test.java
+++ src/Tree/Test.java
@@ -1,2 +1,21 @@
-package Tree;public class Test {
+package Tree;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int[] nums = {6, 2, 9, 4, 8, 1, 3, 5, 7};
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.populate(nums);
+        tree.display();
+
+        System.out.println("\nPre-order : ");
+        tree.preOrder();
+
+        System.out.println("\nIn-order : ");
+        tree.inOrder();
+
+        System.out.println("\nPost-order : ");
+        tree.postOrder();
+    }
+
 }
