fd420f42b752200bb10ba89750c4040ca89a5e30
sneha <snehatarnekar@gmail.com>
2024-05-18 22:27:16 +0530

    Print star pattern11

---
+++ src/Pattern/Pattern11.java
@@ -0,0 +1,35 @@
+package Pattern;
+
+
+/*
+
+ *   *   *   *   *
+   *   *   *   *
+     *   *   *
+       *   *
+         *
+
+ */
+public class Pattern11 {
+
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i * 2; j < n * 2 - 1; j++) {
+                if (j % 2 == 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
