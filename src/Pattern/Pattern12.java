6982085266b5d7f7acc391a1965ff800bbcc1052
sneha <snehatarnekar@gmail.com>
2024-05-26 19:46:02 +0530

    Print pattern12

---
+++ src/Pattern/Pattern12.java
@@ -0,0 +1,55 @@
+package Pattern;
+
+/*
+
+ *   *   *   *   *
+   *   *   *   *
+     *   *   *
+       *   *
+         *
+         *
+       *   *
+     *   *   *
+   *   *   *   *
+ *   *   *   *   *
+
+ */
+public class Pattern12 {
+
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    public static void pattern(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i * 2; j < n * 2 - 1; j++) {
+                if (j % 2 == 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i * 2; j++) {
+                if (j % 2 == 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
